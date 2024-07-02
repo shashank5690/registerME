396d3d0758bd80c3397346317475b00847ea2aa6
Shashank singh <shashank.singh@binmile.com>
2024-07-02 18:28:57 +0530

    login

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -13,7 +13,7 @@ import localforage from 'localforage'; // Import localforage here
 
 const schema = yup.object({
   email: yup.string().email('Invalid email').required('Email is required'),
-  password: yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+  password: yup.string().min(4, 'Password must be at least 4 characters').required('Password is required'),
   roleType: yup.mixed<'user' | 'admin'>().oneOf(['user', 'admin'], 'Role is required').required('Role is required')
 }).required();
 
